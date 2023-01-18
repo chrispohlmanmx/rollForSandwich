42722c7ffb9e33fdd3746637f90cfdcd36cfed96
Chris Pohlman <c.pohlmanmx+github@gmail.com>
2023-01-17 18:30:33 -0600

    got basic sandwich rolling working to the console

--- rollForSandwich/src/App.jsx
+++ rollForSandwich/src/App.jsx
@@ -1,34 +1,152 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+const breadsList = [
+    "White Bread",
+    "Sourdough",
+    "Wheat Bread",
+    "Baguette",
+    "French Bread",
+    "Texas Toast",
+    "Tortilla",
+    "Naan",
+    "Pita",
+    "brioche",
+    "Ciabatta",
+    "Rye",
+    "Multigrain",
+];
+const meatsList = [
+    "Chicken",
+    "Turkey",
+    "Ham",
+    "Roast Beef",
+    "Shredded Chicken",
+    "Shredded Beef",
+    "Pulled Pork",
+    "Mortadella",
+    "Salami",
+    "Pepperoni",
+    "Steak",
+    "Gyro",
+    "Bacon",
+    "Bologna",
+    "Braunschweiger",
+];
+const cheesesList = [
+    "Cheddar",
+    "American",
+    "White American",
+    "Colby",
+    "Colby Jack",
+    "Pepperjack",
+    "Montery Jack",
+    "Mozzarella",
+    "Parmesan",
+    "Muenster",
+    "Havarti",
+    "Provolone",
+    "Gouda",
+    "Smoked Gouda",
+    "Bleu",
+    "Swiss",
+];
+const veggiesList = [
+    "Lettuce",
+    "Onion",
+    "Bell Pepper",
+    "Jalpeno",
+    "Pickles",
+    "Olives",
+    "Mushrooms",
+    "Sprouts",
+    "Spinach",
+    "Roasted Red Peppers",
+    "Giardinera",
+    "Pickled Peppers",
+    "Sweet Peppers",
+    "Hot Peppers",
+    "Tomato",
+    "Carrot",
+    "Broccoli",
+    "Cabbage",
+    "Cauliflower",
+    "Eggplant",
+    "Garlic",
+    "Brussel Sprouts",
+    "Beets",
+];
+const condimentsList = [
+    "BBQ",
+    "Steak Sauce",
+    "Yellow Mustard",
+    "Dijon Mustard",
+    "Spicy Mustard",
+    "Ketchup",
+    "Mayo",
+    "Sriracha",
+    "Aioli",
+    "Oil",
+    "Vinegar",
+    "Marinara",
+    "Chimicurri",
+    "Teriyaki",
+    "Peanut Sauce",
+    "Buffalo Sauce",
+    "Hot Sauce",
+    "Jam",
+    "Jelly",
+    "Pesto",
+    "Hummus",
+    "Cream Cheese",
+    "Butter",
+    "Horseradish",
+    "Honey",
+    "Gochujang",
+    "Salsa",
+    "Guacamole",
+    "Sour Cream",
+    "Harissa",
+    "Baba Ganoush",
+    "Honey Mustard",
+    "Ranch",
+    "Thousand Island Dressing",
+    "Italian Dressing",
+    "Caesar Dressing",
+    "French Dressing",
+    "Relish",
+    "Chutney",
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+    function roll() {
+        var breadsSelection;
+        var meatsSelection;
+        var cheesesSelection;
+        var veggiesSelection;
+        var condimentsSelection;
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+        breadsSelection = breadsList[getRandomInt(breadsList.length)];
+        meatsSelection = meatsList[getRandomInt(meatsList.length)];
+        cheesesSelection = cheesesList[getRandomInt(cheesesList.length)];
+        veggiesSelection = veggiesList[getRandomInt(veggiesList.length)];
+        condimentsSelection =
+            condimentsList[getRandomInt(condimentsList.length)];
+
+        console.log(
+            `${breadsSelection} + ${meatsSelection} + ${cheesesSelection} + ${veggiesSelection} + ${condimentsSelection}`
+        );
+    }
+
+    return (
+        <div className="App">
+            <button onClick={roll}>Roll</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
